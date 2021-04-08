dfeb9299eb5a71b4939eec961c177463b5a73b86
Shu-Xiao Lio <d3a2s1l@gmail.com>
2021-04-09 02:27:07 +0800

    fix type bug

--- alicptfts/UnitTest/connectTest.py
+++ alicptfts/UnitTest/connectTest.py
@@ -38,28 +38,28 @@ op = 0
 
 # op = myXPS.OpenInstrument(host, port,timeout)
 hostIP = '192.168.254.254'
-op = myXPS.OpenInstrument(hostIP, 5001, 1000)
+#op = myXPS.OpenInstrument(hostIP, 5001, 1000)
 if (op != 0): raise ValueError('Error: Could not open XPS for test\nError code: {}'.format(op))
 else: print("Status: connecting to the XPS controller")
 
 # reference code: XPS Unified Programmer's Manual.pdf, Ch.9
 op = myXPS.Login('Administrator','Administrator','DUMMY') ## not sure whether it's required
-if (op != 0): raise ValueError('Error: Could login\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('Error: Could not login\nError code: {}'.format(op))
 else: print("Status: Login XPS")
 
 op = myXPS.KillAll('DUMMY')
-if (op != 0): raise ValueError('Error: Could reset group status\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('Error: Could reset group status\nError code: {}'.format(op))
 else: print("Status: Reset All Groups")
 
 # group: XPS Unified Programmer's Manual.pdf, Ch.5
 op = myXPS.GroupInitialize('SingleAxis','DUMMY')
 op = myXPS.GroupHomeSearch('SingleAxis','DUMMY')
-if (op != 0): raise ValueError('Error: Cannot initialize group\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('Error: Cannot initialize group\nError code: {}'.format(op))
 else: print('Status: Initialize group')
 
 pos1 = Array[float]([0,0])
 op = mmyXPS.GroupMoveAbsolute('SingleAxis',pos1,1,'DUMMY')
-if (op != 0): raise ValueError('GroupMoveAbsolute Error\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('GroupMoveAbsolute Error\nError code: {}'.format(op))
 else: print('Status: Reset the position to origin')
 '''
 Open XPS Connection
@@ -77,8 +77,8 @@ code=myxps.CloseInstrument;
 '''
 pos2 = Array[float]([100,0])
 op = mmyXPS.GroupMoveRelative('SingleAxis',pos2,1,'DUMMY')
-if (op != 0): raise ValueError('GroupMoveRelative Error\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('GroupMoveRelative Error\nError code: {}'.format(op))
 else: print('Status: move to +100 mm')
 op = myXPS.CloseInstrument()
-if (op != 0): raise ValueError('Error: Failure to close XPS\nError code: {}'.format(op))
+if (op[0] != 0): raise ValueError('Error: Failure to close XPS\nError code: {}'.format(op))
 else: print('Status: Close Connection\nTest Finish')
