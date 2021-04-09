69437de61898e5a390eaa5a2f496b5571f282316
Shu-Xiao Lio <d3a2s1l@gmail.com>
2021-04-09 12:43:02 +0800

    Add Spindle group

--- alicptfts/UnitTest/connectTest.py
+++ alicptfts/UnitTest/connectTest.py
@@ -14,6 +14,7 @@ import os
 import time
 from collections import OrderedDict
 from configparser import ConfigParser
+from time import sleep
 #from sftpwrapper import SFTPWrapper
 #import sftpwrapper
 
@@ -30,7 +31,7 @@ clr.AddReference(r'Newport.XPS.CommandInterface')
 from CommandInterfaceXPS import *
 import numpy as np
 
-print('load assembly')
+print('Loading assembly')
 
 # reference code: XPS Unified Programmer's Manual.pdf, p9
 myXPS = XPS()
@@ -51,33 +52,26 @@ if (op[0] != 0): raise ValueError('Error: Could not reset group status\nError co
 else: print("Status: Reset All Groups")
 
 # group: XPS Unified Programmer's Manual.pdf, Ch.5
-op = myXPS.GroupInitialize('SingleAxis','DUMMY')
-op = myXPS.GroupHomeSearch('SingleAxis','DUMMY')
-if (op[0] != 0): raise ValueError('Error: Cannot initialize group\nError code: {}'.format(op))
+#groupName = 'Spindle'
+groupName = 'SingleAxis'
+op = myXPS.GroupInitialize(groupName,'DUMMY')
+if (op[0] != 0): raise ValueError('Error: Could not initialize group\nError code: {}'.format(op))
 else: print('Status: Initialize group')
+op = myXPS.GroupHomeSearch(groupName,'DUMMY')
+if (op[0] != 0): raise ValueError('Error: Could not search home group\nError code: {}'.format(op))
+else: print('Status: Search home group')
 
 pos1 = Array[float]([0,0])
-op = mmyXPS.GroupMoveAbsolute('SingleAxis',pos1,1,'DUMMY')
+op = mmyXPS.GroupMoveAbsolute(groupName,pos1,1,'DUMMY')
 if (op[0] != 0): raise ValueError('GroupMoveAbsolute Error\nError code: {}'.format(op))
 else: print('Status: Reset the position to origin')
-'''
-Open XPS Connection
-% Connect to the XPS controller
-code=myxps.OpenInstrument('192.168.254.254',5001,1000);
-Call XPS Functions
-% Use API's
-[code Version]=myxps.FirmwareVersionGet [code]=myxps.GroupKill('Group1')
-[code]=myxps.GroupInitialize('Group1')
-[code]=myxps.GroupHomeSearch('Group1')
-Close XPS Connection
-% Disconnect from the XPS controller
-code=myxps.CloseInstrument;
 
-'''
+sleep(1)
 pos2 = Array[float]([100,0])
-op = mmyXPS.GroupMoveRelative('SingleAxis',pos2,1,'DUMMY')
+op = mmyXPS.GroupMoveRelative(groupName,pos2,1,'DUMMY')
 if (op[0] != 0): raise ValueError('GroupMoveRelative Error\nError code: {}'.format(op))
 else: print('Status: move to +100 mm')
+
 op = myXPS.CloseInstrument()
 if (op[0] != 0): raise ValueError('Error: Failure to close XPS\nError code: {}'.format(op))
 else: print('Status: Close Connection\nTest Finish')
