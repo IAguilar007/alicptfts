04dac3d376efccf5cbbea104b6f30ef3ca4fe1a5
IAguilar007 <81662195+IAguilar007@users.noreply.github.com>
2021-05-23 20:20:52 -0700

    Update arrowtest.py

--- alicptfts/UnitTest/arrowtest.py
+++ alicptfts/UnitTest/arrowtest.py
@@ -1,15 +1,15 @@
-import logging
+import curses
+from curses import wrapper
 import math
-import time
-
 from newportxps import NewportXPS
-# from newportxps.XPS_C8_drivers import XPSException
-from pynput.keyboard import Listener, Key
+import logging
+import time
+import newportxps.XPS_C8_drivers
 
 # Setup logging
 logging.basicConfig(filename="key_log.txt", level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-xps = NewportXPS('192.168.0.254', username='Administrator', password='Administrator')
+xps = NewportXPS('192.168.0.254', username='xxxxxxx', password='xxxxxx')
 xps.initialize_allgroups()
 xps.home_allgroups()
 
@@ -17,176 +17,93 @@ xps.home_allgroups()
 # Group2: IMS500CC
 # Group3: PR50CC
 
-# INITIAL CONDITIONS
-length1 = 0  # For Group1, mm
-length = 250  # mm
-angle = 45.0  # degrees
-height = 700  # mm
-angle_dependent = True
-fast = False
-
-
-def center_detector(number):  # Finds angle for a given position to hit center detector
-    ideal_angle = math.degrees(math.atan(height / (math.sqrt((number - 250) ** 2 + height ** 2) + number - 250)))
-    return ideal_angle
-
-
-def upper_bound():  # upper boundary for stage 2
-    boundary = 250 + height / math.tan(math.radians(72))  # 72 since 90 - 18
-    return math.trunc(boundary)
-
-
-def lower_bound():  # lower boundary for stage 2
-    boundary = 250 + height / math.tan(math.radians(108))  # 108 since 90 + 18
-    return math.trunc(boundary)
-
-
-def move_stage1():
-    for i in range(15):
-        xps.move_stage('Group1.Pos', 150)
-        xps.move_stage('Group1.Pos', 0)
-
-
 print()
 print('Check Status: Status should be Ready')
 print(xps.status_report())
+time.sleep(5)
 
 print()
 print('Status: Set Max Velocity')  # unit: mm/s
 xps.set_velocity('Group1.Pos', 5)
 xps.set_velocity('Group2.Pos', 5)
 xps.set_velocity('Group3.Pos', 5)
+time.sleep(5)
 
+print()
 print('Status: Set the Initial position')
-xps.move_stage('Group1.Pos', length1)  # Done near top of code
-xps.move_stage('Group2.Pos', length)
-xps.move_stage('Group3.Pos', angle)
-
-print('Use Arrow Keys to move')
-print('Esc button exits program!')
-print('Space button changes angle dependence. Default True')
-print('Enter button changes fast. Default False')
-print('Left control button starts Stage1 motion')
+xps.move_stage('Group1.Pos', 0)  # Initial Conditions
+xps.move_stage('Group2.Pos', 250)
+xps.move_stage('Group3.Pos', 45)
+time.sleep(5)
 
 
-def on_press(key):  # The function that's called when a key is pressed
-    global length, angle, angle_dependent, fast
-    if key == Key.cltr_l:
-        move_stage1()
-    if key == Key.space:  # Space button changes angle dependency
-        angle_dependent = not angle_dependent
-        if angle_dependent:
-            angle = center_detector(length)
-            xps.move_stage('Group3.Pos', round(angle, 2))
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-            logging.info('Stage1: {:.2f} Stage2: {:.2f} Stage3: {:.2f}'.format(xps.get_stage_position(
-                'Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
+def clamp(length, height):
+    upper_boundary = 250 + height / math.tan(math.radians(72))
+    lower_boundary = 250 + height / math.tan(math.radians(108))
+    return math.trunc(max(min(length, upper_boundary), lower_boundary))
+
+
+def center_detector(number, height):
+    ideal_angle = math.degrees(math.atan(height / (math.sqrt((number - 250) ** 2 + height ** 2) + number - 250)))
+    return ideal_angle
+
+
+def main(stdscr):
+    stdscr = curses.initscr()
+    stdscr.clear()
+    length = 250
+    height = 700
+    angle = 45
+    arrow_down = 258
+    arrow_up = 259
+    arrow_left = 260
+    arrow_right = 261
+    angle_independent = False
+    stdscr.addstr(0, 0, f"Position: {round(length, 2)} mm, Angle: {round(angle, 2)}°")
+    stdscr.addstr(1, 0, f"Press arrow keys for motion")
+    stdscr.addstr(2, 0, f"Press 'q' to quit the program")
+    stdscr.addstr(3, 0, f"Press 't' to toggle between an independent angle and a dependent angle")
+    stdscr.addstr(4, 0, f"Mode: Independent angle") if angle_independent else stdscr.addstr(4, 0, f"Mode: Dependent angle")
+    while True:
+        ch = stdscr.getch()
+        stdscr.clear()
+        if ch == ord('q'):
+            stdscr.addstr(0, 0, "Test Completed")
+            stdscr.refresh()
+            xps.KillAll()
+            time.sleep(5)
+            break
+        elif ch == arrow_down and angle_independent:
+            angle -= 0.1
+        elif ch == arrow_up and angle_independent:
+            angle += 0.1
+        elif ch == arrow_left:
+            if length == clamp(length + 1, height):
+                stdscr.addstr(5, 0, "Can't go any more leftwards!")
+            length = clamp(length + 1, height)
+            if not angle_independent:
+                angle = center_detector(length, height)
+        elif ch == arrow_right:
+            if length == clamp(length - 1, height):
+                stdscr.addstr(5, 0, "Can't go any more rightwards!")
+            length = clamp(length - 1, height)
+            if not angle_independent:
+                angle = center_detector(length, height)
+        elif ch == ord('t'):
+            angle_independent = not angle_independent
+            if not angle_independent:
+                angle = center_detector(length, height)
         else:
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-    if key == Key.enter:
-        fast = not fast  # enter key changes how fast this goes
-        print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                             angle_dependent,
-                                                                                                             xps.get_stage_position(
-                                                                                                                 'Group1.Pos'),
-                                                                                                             xps.get_stage_position(
-                                                                                                                 'Group2.Pos'),
-                                                                                                             xps.get_stage_position(
-                                                                                                                 'Group3.Pos')))
-    length_spacing = 1
-    angle_spacing = .1
-    if fast:
-        length_spacing = 10
-        angle_spacing = 1
-    if lower_bound() <= length <= upper_bound():
-        if key == Key.right:
-            if length != upper_bound():
-                length += length_spacing
-            if angle_dependent:
-                angle = center_detector(length)
-            xps.move_stage('Group2.Pos', length)
-            xps.move_stage('Group3.Pos', round(angle, 2))
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-            logging.info('Stage1: {:.2f} Stage2: {:.2f} Stage3: {:.2f}'.format(xps.get_stage_position(
-                'Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
-        if key == Key.left:
-            if length != lower_bound():
-                length -= length_spacing
-            if angle_dependent:
-                angle = center_detector(length)
-            xps.move_stage('Group2.Pos', length)
-            xps.move_stage('Group3.Pos', round(angle, 2))
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-            logging.info('Stage1: {:.2f} Stage2: {:.2f} Stage3: {:.2f}'.format(xps.get_stage_position(
-                'Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
-        if key == Key.up and not angle_dependent:
-            angle += angle_spacing
-            xps.move_stage('Group2.Pos', length)
-            xps.move_stage('Group3.Pos', round(angle, 2))
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-            logging.info('Stage1: {:.2f} Stage2: {:.2f} Stage3: {:.2f}'.format(xps.get_stage_position(
-                'Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
-        if key == Key.down and not angle_dependent:
-            angle -= angle_spacing
-            xps.move_stage('Group2.Pos', length)
-            xps.move_stage('Group3.Pos', round(angle, 2))
-            print('Fast: {}.  Angle Dependent: {}. Stage1: {:.2f} mm  Stage2: {:.2f} mm  Stage3: {:.2f}°'.format(fast,
-                                                                                                                 angle_dependent,
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group1.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group2.Pos'),
-                                                                                                                 xps.get_stage_position(
-                                                                                                                     'Group3.Pos')))
-            logging.info('Stage1: {:.2f} Stage2: {:.2f} Stage3: {:.2f}'.format(xps.get_stage_position(
-                'Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
-    else:
-        length = upper_bound() if length > 10 else lower_bound()
-    if key == Key.esc:  # Esc button exits
-        print('Motion Completed!')
-        print('Status: Print Current Position')
-        print('Stage1: {:.2f}'.format(xps.get_stage_position('Group1.Pos')))
-        print('Stage2: {:.2f}'.format(xps.get_stage_position('Group2.Pos')))
-        print('Stage3: {:.2f}'.format(xps.get_stage_position('Group3.Pos')))
-        print('Test Finish!')
-        time.sleep(10)
-        exit(0)
+            pass
+        xps.move_stage('Group2.Pos', length)
+        xps.move_stage('Group3.Pos', angle)
+        logging.info('Stage1: {:2f} Stage2: {:2f} Stage3: {:2f}'.format(xps.get_stage_position('Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
+        stdscr.addstr(0, 0, 'Stage1: {:2f} mm Stage2: {:2f} mm Stage3: {:2f}°'.format(xps.get_stage_position('Group1.Pos'), xps.get_stage_position('Group2.Pos'), xps.get_stage_position('Group3.Pos')))
+        stdscr.addstr(1, 0, f"Press arrow keys for motion")
+        stdscr.addstr(2, 0, f"Press 'q' to quit the program")
+        stdscr.addstr(3, 0, f"Press 't' to toggle between an independent angle and a dependent angle")
+        stdscr.addstr(4, 0, f"Mode: Independent angle") if angle_independent else stdscr.addstr(4, 0, f"Mode: Dependent angle")
+        stdscr.refresh()
 
 
-with Listener(on_press=on_press) as listener:  # Create an instance of Listener
-    listener.join()  # Join the listener thread to the main thread to keep waiting for keys
+wrapper(main)
