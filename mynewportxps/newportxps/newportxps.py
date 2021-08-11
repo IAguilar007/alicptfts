822cf460e7696cc2c064c341d995416b02d55a35
Tom <tongtianliu@gmail.com>
2021-08-11 13:25:11 -0700

    Update newportxps.py

--- mynewportxps/newportxps/newportxps.py
+++ mynewportxps/newportxps/newportxps.py
@@ -125,6 +125,7 @@ class NewportXPS:
             self.read_systemini()
         except:
             print("Could not read system.ini!!!")
+            raise AttributeError()
 
 
     def check_error(self, err, msg='', with_raise=True):
